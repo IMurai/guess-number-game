a26fed4e109eb890159c5c8c0bde727a81099d13
IMurai <raihaan.tech@gmail.com>
2026-04-25 08:08:04 +0700

    Refactor: rename variable correct to guess and totalCorrect to correct

--- src/guess-number.py
+++ src/guess-number.py
@@ -8,28 +8,28 @@ print("-" * 60)
 
 # ======== Validasi Variabel ======== 
 num = random.randrange(1,50) # Mencari angka random dengan library random
-totalCorrect = 0 # Vaiabel jumlah tebakan yang benar
+correct = 0 # Vaiabel jumlah tebakan yang benar
 
 # ======== Loop Tempat Game Berjalan ======== 
 for i in range(7): # Looping untuk mengatur kesempatan Player untuk menjawab
     try:
-        correct = int(input("Masukan Tebakan Antara 1-50: ")) # Input Tebakan Player
+        guess = int(input("Masukan Tebakan Antara 1-50: ")) # Input Tebakan Player
     except ValueError: # Error handling jika Player memasukan jawaban selain angka
         print("Whoops! Input bukan angka. Coba lagi.")
         continue
     
     # Percabangan jika jawaban benar atau salah
-    if correct > num: # Jika tebakan lebih besar dari jawaban
+    if guess > num: # Jika tebakan lebih besar dari jawaban
         print("Kebesaran! Coba angka yang lebih kecil.")
-    elif correct < num: # Jika tebakan lebih kecil dari jawaban
+    elif guess < num: # Jika tebakan lebih kecil dari jawaban
         print("Kekecilan! Coba angka yang lebih besar.")
     else: # Jika Tebakan Benar
-        totalCorrect += 1 # Mengubah Nilai variabel correct
+        correct += 1 # Mengubah Nilai variabel correct
         break
 
 # ======== Output Setelah Game Selesai ========
 # Percabangan output keberhasilan Player 
-if totalCorrect == 1: # Jika Player berhasil menebak
+if correct == 1: # Jika Player berhasil menebak
     print("\nHoree! Kamu Berhasil Menebaknya.")
 else: # Jika Player gagal menebak
     print("\nSayang sekali, kamu gagal Menebaknya.")
