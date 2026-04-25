aa94b9c0aab76e8d8565a4bd65627a0654dc162b
IMurai <raihaan.tech@gmail.com>
2026-04-25 08:04:24 +0700

    Fix: shadowing variable correct

--- src/guess-number.py
+++ src/guess-number.py
@@ -8,28 +8,28 @@ print("-" * 60)
 
 # ======== Validasi Variabel ======== 
 num = random.randrange(1,50) # Mencari angka random dengan library random
-correct = 0 # Vaiabel jumlah tebakan yang benar
+totalCorrect = 0 # Vaiabel jumlah tebakan yang benar
 
 # ======== Loop Tempat Game Berjalan ======== 
 for i in range(7): # Looping untuk mengatur kesempatan Player untuk menjawab
     try:
-        guess = int(input("Masukan Tebakan Antara 1-50: ")) # Input Tebakan Player
+        correct = int(input("Masukan Tebakan Antara 1-50: ")) # Input Tebakan Player
     except ValueError: # Error handling jika Player memasukan jawaban selain angka
         print("Whoops! Input bukan angka. Coba lagi.")
         continue
     
     # Percabangan jika jawaban benar atau salah
-    if guess > num: # Jika tebakan lebih besar dari jawaban
+    if correct > num: # Jika tebakan lebih besar dari jawaban
         print("Kebesaran! Coba angka yang lebih kecil.")
-    elif guess < num: # Jika tebakan lebih kecil dari jawaban
+    elif correct < num: # Jika tebakan lebih kecil dari jawaban
         print("Kekecilan! Coba angka yang lebih besar.")
     else: # Jika Tebakan Benar
-        guess += 1 # Mengubah Nilai variabel correct
+        totalCorrect += 1 # Mengubah Nilai variabel correct
         break
 
 # ======== Output Setelah Game Selesai ========
 # Percabangan output keberhasilan Player 
-if correct == 1: # Jika Player berhasil menebak
+if totalCorrect == 1: # Jika Player berhasil menebak
     print("\nHoree! Kamu Berhasil Menebaknya.")
 else: # Jika Player gagal menebak
     print("\nSayang sekali, kamu gagal Menebaknya.")
